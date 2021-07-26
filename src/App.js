16ce7c970d7593bbbb556b4765740b9677658bcc
Cindy Liang <hakkacindy@gmail.com>
2021-07-26 01:09:08 +0100

    add map function to expenses items,add state to App.js to render the new added item.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
+import React, {useState} from 'react';
+
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
-import { initialExpenses } from './data';
+// import { initialExpenses } from './data';
+
+const DUMMY_EXPENSES = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    location: 'London'
+  },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+    location: 'London'
+  },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+    location: 'Paris'
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    location: 'New York'
+  },
+];
+
 
 const App = () => {
-  
-  const addExpenseHandler = expense => {
-    console.log('In App.js');
-    console.log(expense);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES)
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) =>{
+      return  [expense, ...prevExpenses]
+    });
   }
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={initialExpenses}/>
+      <Expenses items={expenses}/>
     </div>
   );
 }
